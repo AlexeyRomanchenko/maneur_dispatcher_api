bf00b0bc05e376a05b2899dae3de44d34b566acd
alexeyRomanchenko <romanchenko.alek@mail.ru>
2020-07-27 16:31:53 +0300

    successfully read json file from path

--- AGAT.LocoDispatcher.Web.JsonPasrer/Utils/Parser.cs
+++ AGAT.LocoDispatcher.Web.JsonPasrer/Utils/Parser.cs
@@ -1,4 +1,6 @@
 ﻿using Quartz;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace AGAT.LocoDispatcher.Web.JsonPasrer.Utils
@@ -8,6 +10,29 @@ namespace AGAT.LocoDispatcher.Web.JsonPasrer.Utils
         public async Task Execute(IJobExecutionContext context)
         {
             await ParseToJson("wewe");
+        }
+        protected virtual async Task<string> GetJSONFromFileAsync(string pathToFile)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(pathToFile?.Trim()))
+                {
+                    throw new ArgumentNullException("Path is not valid");
+                }
+                using (StreamReader reader = new StreamReader(pathToFile))
+                {
+                    string json = reader.ReadToEnd();
+                    return await Task.FromResult(json);
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
