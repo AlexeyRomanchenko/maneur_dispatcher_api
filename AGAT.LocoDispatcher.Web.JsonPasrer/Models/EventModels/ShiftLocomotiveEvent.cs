dbdbd2b50d977f6fbe5ecd9edb83a366be19b5f3
alexeyRomanchenko <romanchenko.alek@mail.ru>
2020-07-30 16:39:12 +0300

    get train ids from array

---
+++ AGAT.LocoDispatcher.Web.JsonPasrer/Models/EventModels/ShiftLocomotiveEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace AGAT.LocoDispatcher.Web.JsonPasrer.Models.EventModels
+{
+    public class ShiftLocomotiveEvent: MoveEventBase
+    {
+        public ShiftLocomotiveEvent(string type, int timestamp, string esr, string message, List<string> trains)
+        {
+            this.Type = type;
+            this.Timestamp = timestamp;
+            this.ESR = esr;
+            this.Message = message;
+            this.Trains = trains;
+        }
+        public string ESR { get; set; }
+        public IEnumerable<string> Trains { get; set; }
+    }
+}
