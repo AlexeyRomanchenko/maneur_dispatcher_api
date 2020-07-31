ecd058fa559a91b12b048ae0641691caf9989ebd
alexeyRomanchenko <romanchenko.alek@mail.ru>
2020-07-31 10:17:22 +0300

    add loco shift information

---
+++ AGAT.LocoDispatcher.Business.Data/Helpers/LogicHelpers/LocoShiftHelper.cs
@@ -0,0 +1,58 @@
+﻿using AGAT.LocoDispatcher.Data.Managers;
+using AGAT.LocoDispatcher.Data.Models.EventModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGAT.LocoDispatcher.Data.Helpers.LogicHelpers
+{
+    public class LocoShiftHelper
+    {
+        private DataManager manager;
+        public LocoShiftHelper()
+        {
+            manager = new DataManager();
+        }
+        public async Task AddLocoShiftByNameAsync(LocoShiftEvent loco, int timestamp)
+        {
+            try
+            {
+                if (loco is null)
+                {
+                    throw new ArgumentNullException("Locomotive number is not valid");
+                }
+                LocoShiftEvent locoShift = await manager.shiftRepository.GetActiveByNameAsync(loco.TrainNumber);
+                if (locoShift is null)
+                {
+                    LocoShiftEvent shiftEvent = new LocoShiftEvent
+                    {
+                        CreatedAt = DateTime.Now,
+                        ESR = loco.ESR,
+                        StartShift = ConvertHelper.TimestampToDateTime(timestamp),
+                        TrainNumber = loco.TrainNumber
+                    };
+                    await manager.shiftRepository.CreateAsync(shiftEvent);
+                }
+                else
+                {
+                    locoShift.EndShift = ConvertHelper.TimestampToDateTime(timestamp);
+                    await manager.shiftRepository.UpdateShiftEndAsync(locoShift);
+                    LocoShiftEvent newShiftEvent = new LocoShiftEvent 
+                    {
+                        CreatedAt = DateTime.Now,
+                        ESR = locoShift.ESR,
+                        StartShift = ConvertHelper.TimestampToDateTime(timestamp),
+                        TrainNumber = locoShift.TrainNumber
+                    };
+                    await manager.shiftRepository.CreateAsync(newShiftEvent);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+          
+        }
+    }
+}
